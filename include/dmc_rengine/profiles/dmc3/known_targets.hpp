a65ee707ab54dec155605b208b237aec324e1e80
VrUaCom <rusak.viktor.vr@gmail.com>
2026-08-02 22:54:31 +0200

    feat(dmc3): define canonical executable target registry

---
+++ include/dmc_rengine/profiles/dmc3/known_targets.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "dmc_rengine/exe/known_target.hpp"
+
+namespace dmc::rengine::profiles::dmc3 {
+
+[[nodiscard]] const exe::KnownExecutableTarget& phase12_canonical_target() noexcept;
+
+} // namespace dmc::rengine::profiles::dmc3
