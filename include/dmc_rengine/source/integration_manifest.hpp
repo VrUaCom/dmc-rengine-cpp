7390710dfee7d1f8e93ae249bd965ddc1638ed15
VrUaCom <rusak.viktor.vr@gmail.com>
2026-08-03 09:26:13 +0200

    feat(source): define integration project manifest

---
+++ include/dmc_rengine/source/integration_manifest.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "dmc_rengine/integration/project_workspace.hpp"
+
+#include <string>
+#include <string_view>
+
+namespace dmc::rengine::source {
+
+[[nodiscard]] std::string integration_project_manifest_json(
+    const integration::ProjectWorkspace& workspace,
+    std::string_view project_id);
+
+} // namespace dmc::rengine::source
