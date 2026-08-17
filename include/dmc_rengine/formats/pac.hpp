22e7263cc3025c6f9d6041c5dac320b9a1f9578c
VrUaCom <rusak.viktor.vr@gmail.com>
2026-08-18 00:00:41 +0200

    formats: route PAC through shared relative-slot result contract

--- include/dmc_rengine/formats/pac.hpp
+++ include/dmc_rengine/formats/pac.hpp
@@ -1,35 +1,15 @@
 #pragma once
 
-#include "dmc_rengine/formats/container.hpp"
+#include "dmc_rengine/formats/relative_slot_container.hpp"
 
 #include <cstddef>
 #include <cstdint>
-#include <optional>
 #include <span>
-#include <string>
 
 namespace dmc::rengine::formats {
 
-enum class PacParseError : std::uint8_t {
-    none,
-    truncated_header,
-    invalid_magic,
-    slot_count_limit,
-    truncated_offset_table,
-    slot_offset_before_payload,
-    slot_offset_out_of_bounds,
-    invalid_document,
-};
-
-struct PacParseResult final {
-    std::optional<ContainerDocument> document;
-    PacParseError error{PacParseError::none};
-    std::string message;
-
-    [[nodiscard]] bool ok() const noexcept {
-        return document.has_value() && error == PacParseError::none;
-    }
-};
+using PacParseError = RelativeSlotParseError;
+using PacParseResult = RelativeSlotParseResult;
 
 class PacParser final {
 public:
