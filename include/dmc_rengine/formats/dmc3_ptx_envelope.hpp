87b61d3f3ca8d2642d439846ee99e599ade3c3d5
VrUaCom <rusak.viktor.vr@gmail.com>
2026-08-22 00:16:39 +0200

    Pass 88 model TM2 embedded DDS bridge metadata

--- include/dmc_rengine/formats/dmc3_ptx_envelope.hpp
+++ include/dmc_rengine/formats/dmc3_ptx_envelope.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <span>
@@ -8,12 +9,22 @@
 
 namespace dmc::rengine::formats {
 
+struct Dmc3Tm2DdsBridge {
+  std::uint32_t dds_relative_offset{0};
+  std::uint32_t dds_byte_size{0};
+  std::size_t dds_offset{0};
+  std::uint16_t width{0};
+  std::uint16_t height{0};
+  std::array<std::byte, 0x18> runtime_texture_metadata{};
+  bool has_embedded_dds{false};
+};
+
 struct Dmc3PtxEnvelopeEntry {
   std::uint32_t index{0};
   std::uint32_t block_count{0};
   std::size_t offset{0};
   std::size_t span_size{0};
-  std::uint32_t tim2_data_offset{0};
+  Dmc3Tm2DdsBridge texture;
   bool terminal_span_to_eof{false};
 };
 
@@ -41,7 +52,20 @@ class Dmc3PtxEnvelopeParser final {
   static constexpr std::size_t kHeaderSize = 0x800;
   static constexpr std::size_t kSectorSize = 0x800;
   static constexpr std::size_t kCountTableOffset = 0x04;
+
+  static constexpr std::size_t kTm2DdsRelativeOffsetField = 0x08;
+  static constexpr std::size_t kTm2DdsByteSizeField = 0x3C;
+  static constexpr std::size_t kTm2RuntimeTextureMetadataOffset = 0x50;
+  static constexpr std::size_t kTm2RuntimeTextureMetadataSize = 0x18;
+  static constexpr std::size_t kTm2WidthField = 0x58;
+  static constexpr std::size_t kTm2HeightField = 0x5A;
+
   static constexpr std::uint32_t kTim2Magic = 0x00324D54U;  // "TM2\0", little-endian.
+  static constexpr std::uint32_t kDdsMagic = 0x20534444U;   // "DDS ", little-endian.
+  static constexpr std::uint32_t kDdsHeaderSize = 0x7CU;
+  static constexpr std::uint32_t kDdsPixelFormatSize = 0x20U;
+  static constexpr std::size_t kDdsMinimumBytes = 0x80;
+  static constexpr std::size_t kDdsPixelFormatSizeField = 0x4C;
 
   [[nodiscard]] static Dmc3PtxEnvelopeParseResult parse(std::span<const std::byte> bytes);
 };
