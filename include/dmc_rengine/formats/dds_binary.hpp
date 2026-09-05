62ec50ddfd5235af92c0a1d39933228f210fcff1
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-05 14:43:00 +0200

    formats: add DDS binary adapter interface

---
+++ include/dmc_rengine/formats/dds_binary.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "dmc_rengine/binary/document.hpp"
+#include "dmc_rengine/formats/dds.hpp"
+
+#include <optional>
+#include <span>
+
+namespace dmc::rengine::formats::dds {
+
+[[nodiscard]] std::optional<binary::Document> build_binary_document(
+    gdspaces::ResourceRef resource,
+    std::span<const std::byte> bytes,
+    const ScanResult& scan);
+
+} // namespace dmc::rengine::formats::dds
