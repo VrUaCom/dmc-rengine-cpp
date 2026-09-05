32cb1f18d801fd01853ad83574de2860f2421f31
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-05 14:44:52 +0200

    integration: declare built-in Native Reader modules

---
+++ include/dmc_rengine/integration/native_reader_modules.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "dmc_rengine/integration/native_reader_module.hpp"
+
+namespace dmc::rengine::integration::native_reader_modules {
+
+[[nodiscard]] NativeReaderModule dds();
+[[nodiscard]] NativeReaderModule ptx();
+[[nodiscard]] NativeReaderModule hits();
+[[nodiscard]] NativeReaderModule dca();
+[[nodiscard]] NativeReaderModule lig2();
+[[nodiscard]] NativeReaderModule stage_txt();
+[[nodiscard]] NativeReaderModule scm();
+[[nodiscard]] NativeReaderModule pe();
+
+} // namespace dmc::rengine::integration::native_reader_modules
