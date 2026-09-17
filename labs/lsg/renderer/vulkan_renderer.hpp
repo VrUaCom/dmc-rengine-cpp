629fa3c23b8605114ab1cc73955bd86fbd469d1c
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-17 13:23:51 +0200

    feat(lsg): expose diagnostic render and projection telemetry

--- labs/lsg/renderer/vulkan_renderer.hpp
+++ labs/lsg/renderer/vulkan_renderer.hpp
@@ -7,6 +7,27 @@
 
 namespace rengine::lsg {
 
+enum class DiagnosticRenderMode : std::uint8_t {
+  genome_perspective = 0,
+  raw_perspective = 1,
+  raw_orthographic = 2,
+};
+
+struct RendererDiagnostics {
+  std::uint32_t swapchain_width{};
+  std::uint32_t swapchain_height{};
+  std::uint32_t logical_width{};
+  std::uint32_t logical_height{};
+  std::uint32_t surface_rotation{};
+  float logical_aspect{1.0f};
+  float fov_y_radians{};
+  float camera_distance_m{};
+  float near_plane_m{0.03f};
+  float far_plane_m{30.0f};
+  std::uint64_t estimated_gpu_bytes{};
+  DiagnosticRenderMode mode{DiagnosticRenderMode::genome_perspective};
+};
+
 class VulkanRenderer {
 public:
   struct Impl;
@@ -28,6 +49,10 @@ public:
   void zoom_camera(float scale) noexcept;
   void set_camera_preset(CameraPreset preset) noexcept;
   [[nodiscard]] CameraState camera_state() const noexcept;
+
+  void set_diagnostic_mode(DiagnosticRenderMode mode) noexcept;
+  [[nodiscard]] DiagnosticRenderMode diagnostic_mode() const noexcept;
+  [[nodiscard]] RendererDiagnostics diagnostics() const noexcept;
 
 private:
   std::unique_ptr<Impl> impl_;
