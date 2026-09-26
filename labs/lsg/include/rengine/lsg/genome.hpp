6657281b3d7c3787b7b174905a1895a227db8dd5
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-26 16:07:46 +0200

    labs/lsg: define three built-in Human DNA profiles

--- labs/lsg/include/rengine/lsg/genome.hpp
+++ labs/lsg/include/rengine/lsg/genome.hpp
@@ -8,6 +8,8 @@
 namespace rengine::lsg {
 inline constexpr std::uint16_t kGenomeVersion = 0;
 inline constexpr std::uint32_t kGeneratorRevision = 2;
+inline constexpr std::uint32_t kBuiltinProfileCount = 3;
+inline constexpr std::uint32_t kAdaProfileIndex = 2;
 inline constexpr std::size_t kGenomeHardLimit = 4096;
 
 struct GeometryGenome {
